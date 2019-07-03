a0bd33031c9da866ea45df2154e3e15a15187b93
Brian Pitta <Brian.Pitta@illumio.com>
2019-07-03 12:24:21 -0400

    updated docs

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ import (
 
 var cfgFile, projectBase, userLicense string
 
-var rootCmd = &cobra.Command{
+var RootCmd = &cobra.Command{
 	Use:   "workloader",
 	Short: "Workloader is a tool that helps discover, label, and manage workloads in an Illumio PCE.",
 	Run: func(cmd *cobra.Command, args []string) {
@@ -30,25 +30,25 @@ func init() {
 	cobra.EnableCommandSorting = false
 
 	// Available commands
-	rootCmd.AddCommand(loginCmd)
-	rootCmd.AddCommand(csvCmd)
-	rootCmd.AddCommand(traffic.TrafficCmd)
-	rootCmd.AddCommand(subnet.SubnetCmd)
-	rootCmd.AddCommand(hostname.HostnameCmd)
-	rootCmd.AddCommand(compatabilityCmd)
-	rootCmd.AddCommand(mode.ModeCmd)
+	RootCmd.AddCommand(loginCmd)
+	RootCmd.AddCommand(csvCmd)
+	RootCmd.AddCommand(traffic.TrafficCmd)
+	RootCmd.AddCommand(subnet.SubnetCmd)
+	RootCmd.AddCommand(hostname.HostnameCmd)
+	RootCmd.AddCommand(compatabilityCmd)
+	RootCmd.AddCommand(mode.ModeCmd)
 
 	// Hidden Commands
 	showHidden, _ := strconv.ParseBool(strings.ToLower(os.Getenv("ILLUMIO_ALL")))
 	if showHidden {
-		rootCmd.AddCommand(flowCmd)
+		RootCmd.AddCommand(flowCmd)
 	}
 
 }
 
 // Execute is called by the CLI main function to initiate the Cobra application
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
+	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
