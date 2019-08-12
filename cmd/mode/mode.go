2be27191b95a3185ecc053610f74396fbbfcf464
Brian Pitta <Brian.Pitta@illumio.com>
2019-08-12 12:11:43 -0400

    merged files in mode

--- cmd/mode/mode.go
+++ cmd/mode/mode.go
@@ -1,7 +1,12 @@
 package mode
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"os"
+	"strings"
 
 	"github.com/brian1917/illumioapi"
 	"github.com/brian1917/workloader/utils"
@@ -55,6 +60,61 @@ Use --hrefCol and --stateCol to specify the columns if not default (href=1, stat
 
 		modeUpdate()
 	},
+}
+
+type target struct {
+	workloadHref string
+	targetMode   string
+}
+
+func parseCsv(filename string) []target {
+	// Adjust the columns so first column is  0
+	hrefCol--
+	desiredStateCol--
+
+	// Create our targets slice to hold results
+	var targets []target
+
+	// Open CSV File and create the reader
+	file, err := os.Open(filename)
+	if err != nil {
+		utils.Log(1, fmt.Sprintf("opening CSV - %s", err))
+	}
+	defer file.Close()
+	reader := csv.NewReader(utils.ClearBOM(bufio.NewReader(file)))
+
+	// Start the counter
+	i := 0
+
+	// Iterate through CSV entries
+	for {
+
+		// Read the line
+		line, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			utils.Log(1, fmt.Sprintf("reading CSV file - %s", err))
+		}
+
+		// Increment the counter
+		i++
+
+		// Skipe the header row
+		if i == 1 {
+			continue
+		}
+
+		// Check to make sure we have a valid build state and then append to targets slice
+		m := strings.ToLower(line[desiredStateCol])
+		if m != "idle" && m != "build" && m != "test" && m != "enforced" {
+			utils.Log(1, fmt.Sprintf("invalid mode on line %d - %s not acceptable", i, line[desiredStateCol]))
+		}
+		targets = append(targets, target{workloadHref: line[hrefCol], targetMode: m})
+	}
+
+	return targets
 }
 
 func modeUpdate() {
