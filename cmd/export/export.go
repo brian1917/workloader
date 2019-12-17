bbf2baf6a32231e794620b1fe8cd9d0b32ad0185
Brian Pitta <Brian.Pitta@illumio.com>
2019-12-17 11:14:15 -0500

    changed comment

--- cmd/export/export.go
+++ cmd/export/export.go
@@ -19,7 +19,7 @@ var err error
 var debug bool
 var outFormat string
 
-// ExportCmd runs the workload identifier
+// ExportCmd runs the workload identifier A
 var ExportCmd = &cobra.Command{
 	Use:   "export",
 	Short: "Create a CSV export of all workloads in the PCE.",
