abaeab4c6644a40ef1973009b201c3863de05533
Brian Pitta <Brian.Pitta@illumio.com>
2020-02-21 15:26:11 -0500

    added agent ID to wkld export

--- cmd/wkldexport/wkldexport.go
+++ cmd/wkldexport/wkldexport.go
@@ -47,7 +47,7 @@ func exportWorkloads() {
 	utils.Log(0, "running export command")
 
 	// Start the data slice with headers
-	csvData := [][]string{[]string{"hostname", "role", "app", "env", "loc", "interfaces", "ip_with_default_gw", "netmask_of_ip_with_def_gw", "default_gw", "default_gw_network", "href", "name", "mode", "online", "policy_sync_status", "policy_applied", "policy_received", "policy_refreshed", "last_heartbeat", "hours_since_last_heartbeat", "os_id", "os_details", "ven_version"}}
+	csvData := [][]string{[]string{"hostname", "role", "app", "env", "loc", "interfaces", "ip_with_default_gw", "netmask_of_ip_with_def_gw", "default_gw", "default_gw_network", "href", "name", "mode", "online", "policy_sync_status", "policy_applied", "policy_received", "policy_refreshed", "last_heartbeat", "hours_since_last_heartbeat", "os_id", "os_details", "ven_version", "ven_id"}}
 	stdOutData := [][]string{[]string{"hostname", "role", "app", "env", "loc", "mode"}}
 
 	// GetAllWorkloads
@@ -74,6 +74,7 @@ func exportWorkloads() {
 
 		// Set up variables
 		interfaces := []string{}
+		venID := ""
 		venVersion := ""
 		policySyncStatus := ""
 		policyAppliedAt := ""
@@ -100,7 +101,9 @@ func exportWorkloads() {
 			venVersion = "unmanaged"
 			lastHeartBeat = "unmanaged"
 			hoursSinceLastHB = "unmanaged"
+			venID = "unmanaged"
 		} else {
+			venID = w.Agent.GetID()
 			venVersion = w.Agent.Status.AgentVersion
 			policySyncStatus = w.Agent.Status.SecurityPolicySyncState
 			policyAppliedAt = w.Agent.Status.SecurityPolicyAppliedAt
@@ -119,14 +122,14 @@ func exportWorkloads() {
 		}
 
 		// Append to data slice
-		csvData = append(csvData, []string{w.Hostname, w.GetRole(pce.LabelMapH).Value, w.GetApp(pce.LabelMapH).Value, w.GetEnv(pce.LabelMapH).Value, w.GetLoc(pce.LabelMapH).Value, strings.Join(interfaces, ";"), w.GetIPWithDefaultGW(), w.GetNetMaskWithDefaultGW(), w.GetDefaultGW(), w.GetNetworkWithDefaultGateway(), w.Href, w.Name, w.GetMode(), online, policySyncStatus, policyAppliedAt, poicyReceivedAt, policyRefreshAt, lastHeartBeat, hoursSinceLastHB, w.OsID, w.OsDetail, venVersion})
+		csvData = append(csvData, []string{w.Hostname, w.GetRole(pce.LabelMapH).Value, w.GetApp(pce.LabelMapH).Value, w.GetEnv(pce.LabelMapH).Value, w.GetLoc(pce.LabelMapH).Value, strings.Join(interfaces, ";"), w.GetIPWithDefaultGW(), w.GetNetMaskWithDefaultGW(), w.GetDefaultGW(), w.GetNetworkWithDefaultGateway(), w.Href, w.Name, w.GetMode(), online, policySyncStatus, policyAppliedAt, poicyReceivedAt, policyRefreshAt, lastHeartBeat, hoursSinceLastHB, w.OsID, w.OsDetail, venVersion, venID})
 		stdOutData = append(stdOutData, []string{w.Hostname, w.GetRole(pce.LabelMapH).Value, w.GetApp(pce.LabelMapH).Value, w.GetEnv(pce.LabelMapH).Value, w.GetLoc(pce.LabelMapH).Value, w.GetMode()})
 	}
 
 	if len(csvData) > 1 {
 		utils.WriteOutput(csvData, stdOutData, fmt.Sprintf("workloader-export-%s.csv", time.Now().Format("20060102_150405")))
 		fmt.Printf("\r\n%d workloads exported.\r\n", len(csvData)-1)
-		fmt.Println("Note - the CSV export will include additional columns: interfaces, default_gw, href, name, online, os, and ven version.")
+		fmt.Println("Note - the CSV export will include additional columns: interfaces, default_gw, href, name, online, os, ven version, and ven id.")
 		utils.Log(0, fmt.Sprintf("export complete - %d workloads exported", len(csvData)-1))
 	} else {
 		// Log command execution for 0 results
