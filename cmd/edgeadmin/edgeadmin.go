8027cfb3ba9b3767b82e765596a5083ee1ceb794
jeff.schmitz <jeff.schmitz@illumio.com>
2021-03-14 19:52:35 -0400

    Another update

--- cmd/edgeadmin/edgeadmin.go
+++ cmd/edgeadmin/edgeadmin.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/brian1917/illumioapi"
 	"github.com/brian1917/workloader/cmd/iplimport"
 	"github.com/brian1917/workloader/cmd/wkldimport"
 
@@ -15,7 +16,7 @@ import (
 )
 
 var debug, updatePCE, noPrompt, doNotProvision bool
-var csvFile, fromPCE, toPCE, outputFileName, edgeGroup string
+var csvFile, fromPCE, toPCE, outputFileName, edgeGroup, coreApp, coreEnv, coreLoc string
 
 func init() {
 	EdgeAdminCmd.Flags().StringVarP(&fromPCE, "from-pce", "f", "", "Name of the PCE with the existing Edge Group to copy over. Required")
@@ -23,6 +24,12 @@ func init() {
 	EdgeAdminCmd.Flags().StringVarP(&toPCE, "to-pce", "t", "", "Name of the PCE to receive Edge Admin Group endpoint info. Only required if using --update-pce flag")
 	EdgeAdminCmd.Flags().StringVarP(&edgeGroup, "edge-group", "g", "", "Name of the Edge group to be copied to Core PCE")
 	EdgeAdminCmd.MarkFlagRequired("edge-group")
+	EdgeAdminCmd.Flags().StringVarP(&coreApp, "core-app", "a", "", "Name of the Edge group to be copied to Core PCE")
+	EdgeAdminCmd.MarkFlagRequired("core-app")
+	EdgeAdminCmd.Flags().StringVarP(&coreEnv, "core-env", "e", "", "Name of the Edge group to be copied to Core PCE")
+	EdgeAdminCmd.MarkFlagRequired("core-env")
+	EdgeAdminCmd.Flags().StringVarP(&coreLoc, "core-loc", "l", "", "Name of the Edge group to be copied to Core PCE")
+	EdgeAdminCmd.MarkFlagRequired("core-loc")
 	EdgeAdminCmd.Flags().BoolVarP(&doNotProvision, "do-not-prov", "x", false, "Do not provision created/updated IP Lists. Will provision by default.")
 	EdgeAdminCmd.Flags().StringVar(&outputFileName, "output-file", "", "optionally specify the name of the output file location. default is current location with a timestamped filename.")
 
@@ -71,30 +78,68 @@ func edgeadmin() {
 		utils.LogError(err.Error())
 	}
 
-	label, a, err := sPce.GetLabelbyKeyValue("role", edgeGroup)
+	//get Edge group Href for workload filter
+	slabel, a, err := sPce.GetLabelbyKeyValue("role", edgeGroup)
 	utils.LogAPIResp("GetLabelbyKeyValue", a)
 	if err != nil {
 		utils.LogError(err.Error())
 	}
-	if label.Value == "" {
-		utils.LogError(fmt.Sprintf("erro finding Edge group - %s", edgeGroup))
+	//Make sure label is not empty...have user reenter if so
+	if slabel.Value == "" {
+		utils.LogError(fmt.Sprintf("error finding Edge group - %s.  Please reenter with exact Edge group name", edgeGroup))
 	}
 
-	queryP := map[string]string{"labels": fmt.Sprintf("[[\"%s\"]]", label.Href)}
+	queryP := map[string]string{"labels": fmt.Sprintf("[[\"%s\"]]", slabel.Href)}
 	// Get all workloads from the source PCE
-	wklds, a, err := sPce.GetAllWorkloadsQP(queryP)
+	swklds, a, err := sPce.GetAllWorkloadsQP(queryP)
 	utils.LogAPIResp("GetAllWorkloads", a)
 	if err != nil {
 		utils.LogError(err.Error())
 	}
-	var input wkldimport.Input
-	csvOut := [][]string{{"hostname", "name", "role", "app", "env", "loc", "interfaces", "public_ip", "ip_with_default_gw", "netmask_of_ip_with_def_gw", "default_gw", "default_gw_network", "href", "description", "policy_state", "online", "agent_status", "security_policy_sync_state", "security_policy_applied_at", "security_policy_received_at", "security_policy_refresh_at", "last_heartbea_on", "hours_since_last_heartbeat", "os_id", "os_detail", "agent_version", "agent_id", "active_pce_fqdn", "service_provider", "data_center", "data_center_zone", "cloud_instance_id", "external_data_set", "external_data_reference"}}
-	stdOut := [][]string{{"hostname", "role", "app", "env", "loc", "mode"}}
 
-	for _, w := range wklds {
+	var dPce illumioapi.PCE
+	if toPCE != "" {
+
+		// Get the source pce
+		dPce, err = utils.GetPCEbyName(toPCE, true)
+		if err != nil {
+			utils.LogError(err.Error())
+		}
+		//Get all existing workloads already on dest PCE to check if there updates needed.
+		var labelfilter []string
+		var destvalues = []string{slabel.Value, coreApp, coreEnv, coreLoc}
+		var destkeys = []string{"role", "app", "env", "loc"}
+		for i, l := range destvalues {
+			dlabel, a, err := dPce.GetLabelbyKeyValue(destkeys[i], l)
+			utils.LogAPIResp("GetLabelbyKeyValue", a)
+			if err != nil {
+				utils.LogError(err.Error())
+			}
+			if dlabel.Href != "" {
+				labelfilter = append(labelfilter, dlabel.Href)
+			}
+		}
+		//create filter to get all workloads that are unmanaged and already labe the group and set labels,
+		tmpstr := "[[\"" + strings.Join(labelfilter, "\",\"") + "\"]]"
+		queryP := map[string]string{"labels": tmpstr}
+		queryP["managed"] = "false"
+		// Get all workloads from the destination PCE
+		dwklds, a, err := dPce.GetAllWorkloadsQP(queryP)
+		utils.LogAPIResp("GetAllWorkloads", a)
+		if err != nil {
+			utils.LogError(err.Error())
+		}
+		fmt.Print(dwklds)
+	}
+
+	var input wkldimport.Input
+	csvOut := [][]string{{"hostname", "name", "role", "app", "env", "loc", "interfaces", "public_ip", "href", "description", "data_center", "external_data_set", "external_data_reference", "distinguished_name"}}
+	stdOut := [][]string{{"hostname", "role", "app", "env", "loc", "distinguished_name"}}
+
+	for _, w := range swklds {
 
 		// Output the CSV
-		if len(wklds) > 0 {
+		if len(swklds) > 0 {
 			// Skip deleted workloads
 			if *w.Deleted {
 				continue
@@ -110,47 +155,7 @@ func edgeadmin() {
 				interfaces = append(interfaces, ipAddress)
 			}
 
-			// Assume the VEN-dependent fields are unmanaged
-			policySyncStatus := "unmanaged"
-			policyAppliedAt := "unmanaged"
-			poicyReceivedAt := "unmanaged"
-			policyRefreshAt := "unmanaged"
-			venVersion := "unmanaged"
-			lastHeartBeat := "unmanaged"
-			hoursSinceLastHB := "unmanaged"
-			venID := "unmanaged"
-			pairedPCE := "unmanaged"
-			agentStatus := "unmanaged"
-			instanceID := "unmanaged"
-			// If it is managed, get that information
-			if w.Agent != nil && w.Agent.Href != "" {
-				policySyncStatus = w.Agent.Status.SecurityPolicySyncState
-				policyAppliedAt = w.Agent.Status.SecurityPolicyAppliedAt
-				poicyReceivedAt = w.Agent.Status.SecurityPolicyReceivedAt
-				policyRefreshAt = w.Agent.Status.SecurityPolicyRefreshAt
-				venVersion = w.Agent.Status.AgentVersion
-				lastHeartBeat = w.Agent.Status.LastHeartbeatOn
-				hoursSinceLastHB = fmt.Sprintf("%f", w.HoursSinceLastHeartBeat())
-				venID = w.Agent.GetID()
-				pairedPCE = w.Agent.ActivePceFqdn
-				if pairedPCE == "" {
-					pairedPCE = sPce.FQDN
-				}
-				agentStatus = w.Agent.Status.Status
-				instanceID = w.Agent.Status.InstanceID
-				if instanceID == "" {
-					instanceID = "NA"
-				}
-			}
-
-			// Set online status
-			var online string
-			if w.GetMode() == "unmanaged" {
-				online = "unmanaged"
-			} else {
-				online = strconv.FormatBool(w.Online)
-			}
-			csvOut = append(csvOut, []string{w.Hostname, w.Name, w.GetRole(sPce.LabelMapH).Value, "app", "env", "loc", strings.Join(interfaces, ";"), w.PublicIP, w.GetIPWithDefaultGW(), w.GetNetMaskWithDefaultGW(), w.GetDefaultGW(), w.GetNetworkWithDefaultGateway(), w.Href, w.Description, w.GetMode(), online, agentStatus, policySyncStatus, policyAppliedAt, poicyReceivedAt, policyRefreshAt, lastHeartBeat, hoursSinceLastHB, w.OsID, w.OsDetail, venVersion, venID, pairedPCE, w.ServiceProvider, w.DataCenter, w.DataCenterZone, instanceID, w.ExternalDataSet, w.ExternalDataReference})
+			csvOut = append(csvOut, []string{w.Hostname, w.Name, w.GetRole(sPce.LabelMapH).Value, "app", "env", "loc", strings.Join(interfaces, ";"), w.PublicIP, w.Description, w.OsID, w.OsDetail, w.DataCenter, w.ExternalDataSet, w.Hostname + w.Href, w.DistinguishedName})
 			stdOut = append(stdOut, []string{w.Hostname, w.GetRole(sPce.LabelMapH).Value, "app", "env", "loc", w.GetMode()})
 		} else {
 			utils.LogInfo("no Workloads created.", true)
@@ -166,17 +171,13 @@ func edgeadmin() {
 	// If updatePCE is disabled, we are just going to alert the user what will happen and log
 	if !updatePCE {
 		utils.LogInfo(fmt.Sprintln("See the output file for IP Lists that would be created. Run again using --to-pce and --update-pce flags to create the IP lists."), true)
-		utils.LogEndCommand("wkld-to-ipl")
+		utils.LogEndCommand("edge-admin")
 		return
 	}
 
 	// If we get here, create the workloads on dest PCE using wkld-import
-	utils.LogInfo(fmt.Sprintf("calling workloader ipl-import to import %s to %s", outputFileName, toPCE), true)
-	dPce, err := utils.GetPCEbyName(toPCE, false)
-	if err != nil {
-		utils.LogError(fmt.Sprintf("error getting to pce - %s", err))
-	}
+	utils.LogInfo(fmt.Sprintf("calling workloader edge-admin to import %s to %s", outputFileName, toPCE), true)
 	iplimport.ImportIPLists(dPce, outputFileName, updatePCE, noPrompt, debug, !doNotProvision)
 
-	utils.LogEndCommand("wkld-to-ipl")
+	utils.LogEndCommand("edge-admin")
 }
