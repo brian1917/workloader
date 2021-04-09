534abd438a416d227dbe4d95df6ad198437f4c08
Brian Pitta <Brian.Pitta@illumio.com>
2021-04-08 20:22:06 -0400

    updated

--- cmd/templatelist/cmd.go
+++ cmd/templatelist/cmd.go
@@ -3,6 +3,7 @@ package templatelist
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"sort"
 	"strings"
 
@@ -21,29 +22,34 @@ var TemplateListCmd = &cobra.Command{
 	Use:   "template-list",
 	Short: "List templates available in",
 	Long: `
-Create a CSV export of all workloads in the PCE. The update-pce and --no-prompt flags are ignored for this command.
+List available Illumio templates.
+
+Segmentation templates are a set of CSV files. By default, workloader looks for an "illumio-template" directory in the current directory. To use a different directory, use the --directory flag.
 
 The update-pce and --no-prompt flags are ignored for this command.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		// Set the directory
+		// Get the directory
 		if directory == "" {
 			directory = "illumio-templates/"
+		} else if directory[len(directory)-1:] != string(os.PathSeparator) {
+			directory = fmt.Sprintf("%s%s", directory, string(os.PathSeparator))
 		}
+
+		// Get the files in that directory
 		files, err := ioutil.ReadDir(directory)
 		if err != nil {
 			utils.LogError(err.Error())
 		}
-		fileNames := []string{}
-		for _, f := range files {
-			fileNames = append(fileNames, strings.Replace(f.Name(), "illumio-template-", "", -1))
-		}
 
+		// Create a template map
 		templates := make(map[string][]string)
 
-		for _, f := range fileNames {
-			templateType := strings.Split(f, "-")[0]
-			templateName := strings.Replace(strings.Replace(f, templateType+"-", "", -1), ".csv", "", -1)
+		// Iterate through each file
+		for _, f := range files {
+			templateType := strings.Split(f.Name(), ".")[1]
+			templateName := strings.Split(f.Name(), ".")[0]
+			// If the templateName is already in the map, append. Else, add it to map.
 			if val, ok := templates[templateName]; ok {
 				templates[templateName] = append(val, templateType)
 			} else {
@@ -51,6 +57,7 @@ The update-pce and --no-prompt flags are ignored for this command.`,
 			}
 		}
 
+		// Create a templateNames slice so we can sort it.
 		templateNames := []string{}
 		for t := range templates {
 			templateNames = append(templateNames, t)
@@ -59,6 +66,7 @@ The update-pce and --no-prompt flags are ignored for this command.`,
 			return i < j
 		})
 
+		// Print the sorted templates and the template types from the map
 		for _, t := range templateNames {
 			fmt.Printf("%s (%s)\r\n", t, strings.Join(templates[t], ", "))
 		}
