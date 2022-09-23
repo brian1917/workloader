d24a0f077017e9f4c40acba726d91f9a2021fad8
Brian Pitta <brianpitta@gmail.com>
2022-09-23 06:44:37 -0400

    improvements to unpair command

--- cmd/unpair/unpair.go
+++ cmd/unpair/unpair.go
@@ -1,11 +1,7 @@
 package unpair
 
 import (
-	"bufio"
-	"encoding/csv"
 	"fmt"
-	"io"
-	"os"
 	"strings"
 	"time"
 
@@ -17,15 +13,16 @@ import (
 
 // Set global variables for flags
 var hrefFile, role, app, env, loc, restore, outputFileName string
-var debug, updatePCE, noPrompt, setLabelExcl, includeOnline, single bool
+var updatePCE, noPrompt, setLabelExcl, includeOnline, singleGetWkld, singleUnpair bool
 var hoursSinceLastHB int
 var pce illumioapi.PCE
 var err error
 
 // Init handles flags
 func init() {
-	UnpairCmd.Flags().StringVar(&restore, "restore", "", "Restore value. Must be saved, default, or disable.")
+	UnpairCmd.Flags().StringVar(&restore, "restore", "saved", "Restore value. Must be saved, default, or disable.")
 	UnpairCmd.Flags().StringVarP(&hrefFile, "href", "f", "", "Location of file with HREFs to be used instead of starting with all workloads.")
+	UnpairCmd.Flags().BoolVar(&singleGetWkld, "single-get-wkld", false, "get workloads in a host file by a single API call vs. bulk API.")
 	UnpairCmd.Flags().StringVarP(&role, "role", "r", "", "Role Label. Blank means all roles.")
 	UnpairCmd.Flags().StringVarP(&app, "app", "a", "", "Application Label. Blank means all applications.")
 	UnpairCmd.Flags().StringVarP(&env, "env", "e", "", "Environment Label. Blank means all environments.")
@@ -33,7 +30,7 @@ func init() {
 	UnpairCmd.Flags().BoolVarP(&setLabelExcl, "exclude-labels", "x", false, "Use provided label filters as excludes.")
 	UnpairCmd.Flags().IntVar(&hoursSinceLastHB, "hours", 0, "Hours since last heartbeat. No value (i.e., 0) will ignore heartbeats.")
 	UnpairCmd.Flags().BoolVar(&includeOnline, "include-online", false, "Include workloads that are online. By default only offline workloads that meet criteria will be unpaired.")
-	UnpairCmd.Flags().BoolVar(&single, "single", false, "One API call per unpair versus one API call per 1000 workloads. This will be dramatically slower, but provide more details in syslog messages.")
+	UnpairCmd.Flags().BoolVar(&singleUnpair, "single-unpair", false, "One API call per unpair versus one API call per 1000 workloads. This will be significantly slower but provide more details in the PCE's syslog messages.")
 	UnpairCmd.Flags().StringVar(&outputFileName, "output-file", "", "optionally specify the name of the output file location. default is current location with a timestamped filename.")
 
 	UnpairCmd.Flags().SortFlags = false
@@ -67,7 +64,6 @@ Use --update-pce and --no-prompt to run unpair with no prompts.`,
 		}
 
 		// Get persistent flags from Viper
-		debug = viper.Get("debug").(bool)
 		updatePCE = viper.Get("update_pce").(bool)
 		noPrompt = viper.Get("no_prompt").(bool)
 
@@ -81,61 +77,88 @@ func unpair() {
 
 	// Check that we aren't unpairing the whole PCE
 	if app == "" && role == "" && env == "" && loc == "" && hoursSinceLastHB == 0 && hrefFile == "" {
-		utils.LogError("Must provide labels, hours, or an input file.")
+		utils.LogError("must provide labels, hours, or an input file.")
 	}
 
 	// Check the restore value
 	restore = strings.ToLower(restore)
 	if restore != "saved" && restore != "default" && restore != "disable" {
-		utils.LogError("Restore value must be saved, default, or disable.")
+		utils.LogError("restore value must be saved, default, or disable.")
 	}
 
-	// Get all workloads
-	allWklds, a, err := pce.GetAllWorkloads()
-	if debug {
-		utils.LogAPIResp("GetAllWorkloads", a)
-	}
-	if err != nil {
-		utils.LogError(err.Error())
+	// Check invalid flag combinations
+	if singleGetWkld && hrefFile == "" {
+		utils.LogError("single-get-wkld flag requires an href file")
 	}
 
-	// Create our wkld slice that will either be all workloads or the workloads that match the HREF input
-	var wklds []illumioapi.Workload
-
-	// If we are given a hrefFile, parse that.
+	// If we have an hrefFile, process it
+	var hrefFileData [][]string
 	if hrefFile != "" {
-		// Create our href list
-		csvWklds := make(map[string]bool)
-
-		// Open CSV File
-		csvFile, _ := os.Open(hrefFile)
-		reader := csv.NewReader(bufio.NewReader(csvFile))
-
-		// Cycle through CSV to add to it.
-		for {
-			line, err := reader.Read()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				utils.LogError(fmt.Sprintf("Reading CSV File - %s", err))
-			}
-			csvWklds[line[0]] = true
+		hrefFileData, err = utils.ParseCSV(hrefFile)
+		if err != nil {
+			utils.LogError(err.Error())
 		}
-
-		// Create our wklds slice
-		for _, w := range allWklds {
-			if csvWklds[w.Href] {
-				wklds = append(wklds, w)
-			}
-		}
-	} else {
-		// If we don't have an HREF file, all workloads is our wkld slice
-		wklds = allWklds
 	}
 
-	// Create our targetWklds slice
-	var targetWklds []illumioapi.Workload
+	// Set workload variables
+	var wklds, targetWklds []illumioapi.Workload
+	csvWklds := make(map[string]bool)
+
+	// Process singleGetWkld - we know href file is provided because already checked above
+	if singleGetWkld {
+		// Iterate through file
+		for rowIndex, row := range hrefFileData {
+			// If singleGetWkld, make api call
+			if strings.Contains(row[0], "/orgs/") {
+				wkld, a, _ := pce.GetWkldByHref(row[0])
+				utils.LogAPIResp("GetWkldByHref", a)
+				// If the status code is in the 400s log it and skip
+				if a.StatusCode >= 400 && a.StatusCode <= 499 {
+					utils.LogWarning(fmt.Sprintf("href file line %d - %s is not a workload - %d status code. skipping.", rowIndex+1, row[0], a.StatusCode), true)
+					// If the status code is not in the 400s and there is an error, error out
+				} else if err != nil {
+					utils.LogError(err.Error())
+					// No error, then add to workloads slice
+				} else {
+					wklds = append(wklds, wkld)
+					utils.LogInfo(fmt.Sprintf("href file line %d of %d - %s - status code: %d", rowIndex+1, len(hrefFileData), row[0], a.StatusCode), true)
+				}
+			}
+		}
+	}
+
+	// Get all managed workloads if single-get-wkld is false
+	if !singleGetWkld {
+		// Fill csv map in case needed
+		for _, row := range hrefFileData {
+			// Add to csvWklds map
+			csvWklds[row[0]] = true
+		}
+
+		// Get all managed workloads
+		allManagedWklds, a, err := pce.GetWklds(map[string]string{"managed": "true"})
+		utils.LogAPIResp("GetAllWorkloads", a)
+		if err != nil {
+			utils.LogError(err.Error())
+		}
+		// If there is an href file, iterate through the hrefData and check if the workloads exists.
+		if hrefFile != "" {
+			for rowIndex, row := range hrefFileData {
+				// If it does not exist, warn (if it is a valid href)
+				if val, exists := pce.Workloads[row[0]]; !exists {
+					if strings.Contains(row[0], "/orgs/") {
+						utils.LogWarning(fmt.Sprintf("href file line %d - %s is not a workload. skipping.", rowIndex+1, row[0]), true)
+					}
+				} else {
+					// If it does exist, add it to the slice
+					wklds = append(wklds, val)
+				}
+			}
+		} else {
+			// No href file, use all the managed workloads
+			wklds = allManagedWklds
+		}
+	}
 
 	// Confirm it's not unmanaged and check the labels to find our matches.
 	for _, w := range wklds {
@@ -178,7 +201,7 @@ func unpair() {
 	}
 
 	// If there are more than 0 workloads, build the data slice for writing
-	data := [][]string{[]string{"hostname", "href", "role", "app", "env", "loc", "policy_sync_status", "last_heartbeat", "hours_since_last_heartbeat"}}
+	data := [][]string{{"hostname", "href", "role", "app", "env", "loc", "policy_sync_status", "last_heartbeat", "hours_since_last_heartbeat"}}
 	for _, t := range targetWklds {
 		// Reset the time value
 		hoursSinceLastHB := ""
@@ -221,7 +244,7 @@ func unpair() {
 	}
 
 	// If single
-	if single {
+	if singleUnpair {
 		// Create a slice of slices
 		singleTargetWklds := [][]illumioapi.Workload{}
 		for _, w := range targetWklds {
