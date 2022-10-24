638665013cf6e8e235a66774a782d0a49b03dd7f
Brian Pitta <brianpitta@gmail.com>
2022-10-24 19:43:15 -0400

    version 8.28.9 improves process-export

--- cmd/processexport/cmd.go
+++ cmd/processexport/cmd.go
@@ -3,6 +3,7 @@ package processexport
 import (
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/brian1917/illumioapi"
@@ -14,10 +15,13 @@ import (
 // Declare local global variables
 var pce illumioapi.PCE
 var err error
-var outputFileName string
+var hrefFile, enforcementMode, outputFileName string
 
 func init() {
+	ProcessExportCmd.Flags().StringVarP(&hrefFile, "href", "f", "", "location of file with hrefs to be used instead of starting with all workloads. header optional")
+	ProcessExportCmd.Flags().StringVar(&enforcementMode, "enforcement-mode", "", "optionally specify an enforcement mode filter. acceptable values are idle, visibility_only, selective, and full. ignored if href file is provided")
 	ProcessExportCmd.Flags().StringVar(&outputFileName, "output-file", "", "optionally specify the name of the output file location. default is current location with a timestamped filename.")
+	ProcessExportCmd.Flags().SortFlags = false
 }
 
 // IplExportCmd runs the workload identifier
@@ -45,30 +49,60 @@ func ExportProcesses(pce illumioapi.PCE, outputFileName string) {
 	// Log command execution
 	utils.LogStartCommand("process-export")
 
-	// Get all managed workloads
-	managedWklds, a, err := pce.GetWklds(map[string]string{"managed": "true"})
-	utils.LogAPIResp("GetAllWorklaodsQP", a)
-	if err != nil {
-		utils.LogError(err.Error())
+	// Validate enforcement mdoe
+	enforcementMode = strings.ToLower(enforcementMode)
+	if enforcementMode != "" && enforcementMode != "idle" && enforcementMode != "visibility_only" && enforcementMode != "full" && enforcementMode != "selective" {
+		utils.LogError("invalid enforcement mode. must be blank, idle, visibility_only, selective, or full.")
+	}
+
+	// Setup some variables
+	var wkldHrefs []string
+
+	// If the hrefFile is provided, parse thar
+	if hrefFile != "" {
+		hrefCsvData, err := utils.ParseCSV(hrefFile)
+		if err != nil {
+			utils.LogError(err.Error())
+		}
+		for _, row := range hrefCsvData {
+			if strings.Contains(row[0], "/orgs/") {
+				wkldHrefs = append(wkldHrefs, row[0])
+			}
+		}
+	}
+
+	if hrefFile == "" {
+		queryParameters := map[string]string{"managed": "true"}
+		if enforcementMode != "" {
+			queryParameters["enforcement_mode"] = enforcementMode
+		}
+		wklds, api, err := pce.GetWklds(queryParameters)
+		utils.LogAPIResp("GetWklds", api)
+		if err != nil {
+			utils.LogError(err.Error())
+		}
+		for _, w := range wklds {
+			wkldHrefs = append(wkldHrefs, w.Href)
+		}
+
 	}
 
 	// Setup CSV Data
-	csvData := [][]string{{"hostname", "process_name", "service_name", "port", "proto"}}
+	csvData := [][]string{{"hostname", "href", "process_name", "service_name", "port", "proto"}}
 
 	// Set up slice of workloads
-	for _, mw := range managedWklds {
-		w, a, err := pce.GetWkldByHref(mw.Href)
-		utils.LogAPIResp("GetAllWorklaodsQP", a)
+	for _, wHref := range wkldHrefs {
+		w, a, err := pce.GetWkldByHref(wHref)
+		utils.LogAPIResp("GetWkldByHref", a)
 		if err != nil {
-			utils.LogError(err.Error())
+			utils.LogWarning(fmt.Sprintf("error getting %s - skipping", wHref), true)
 		}
 		if w.Services == nil || w.Services.OpenServicePorts == nil {
 			continue
 		}
 		for _, osp := range w.Services.OpenServicePorts {
-			csvData = append(csvData, []string{w.Hostname, osp.ProcessName, osp.WinServiceName, strconv.Itoa(osp.Port), strconv.Itoa(osp.Protocol)})
+			csvData = append(csvData, []string{w.Hostname, w.Href, osp.ProcessName, osp.WinServiceName, strconv.Itoa(osp.Port), strconv.Itoa(osp.Protocol)})
 		}
-
 	}
 
 	if len(csvData) > 1 {
