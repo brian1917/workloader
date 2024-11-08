07f95ab507fdc08a296de7169c388845bee98c1f
jdschmitz@yahoo.com <jdschmitz@yahoo.com>
2024-11-08 13:36:31 -0500

    Getting networkdevice updates into workloader

--- cmd/wkldimport/interface.go
+++ cmd/wkldimport/interface.go
@@ -83,7 +83,7 @@ func publicIPIsValid(ip string) bool {
 
 func (w *importWkld) interfaces(input Input) {
 	// If the workload is no unmanaged
-	if w.wkld.GetMode() != "unmanaged" {
+	if !(w.wkld.GetMode() == "unmanaged" && ((w.wkld.Agent != nil && w.wkld.Agent.Type != nil && *w.wkld.Agent.Type != "NetworkEnforcementNode") || (w.wkld.VEN != nil && w.wkld.VEN.Name != nil && strings.Contains(*w.wkld.VEN.Name, "Illumio Network Enforcement Node")))) {
 		// Validate the workload can be updated
 		//if !(w.wkld.GetMode() == "unmanaged" && ((w.wkld.Agent.Type != nil && *w.wkld.Agent.Type != "NetworkEnforcementNode") || (w.wkld.VEN.Name != nil && strings.Contains(*w.wkld.VEN.Name, "Illumio Network Enforcement Node")))) {
 		return
